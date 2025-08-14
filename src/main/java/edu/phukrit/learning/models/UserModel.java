f0e2cb54e0912bf1ecebfe4f4e4249ae393f48c1
Phukrit Kittinontana <phukrit7171@gmail.com>
2025-08-15 04:12:38 +0700

    add : model

---
+++ src/main/java/edu/phukrit/learning/models/UserModel.java
@@ -0,0 +1,60 @@
+package edu.phukrit.learning.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class UserModel {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String fname;
+
+    @Column(name = "last_name")
+    private String lname;
+
+    public UserModel() {
+    }
+
+    public UserModel(String fname, String lname) {
+        this.fname = fname;
+        this.lname = lname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    @Override
+    public String toString() {
+        return "UserModel [id=" + id + ", fname=" + fname + ", lname=" + lname + "]";
+    }
+}
