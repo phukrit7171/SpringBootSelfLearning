e7ab1d87aacecc2cc664a828393d8ae1d3b65bc2
Mr.Phukrit Kittinontana <phukrit7171@gmail.com>
2025-08-14 20:26:10 +0700

    init new env with maven

---
+++ src/test/java/edu/phukrit/learning/LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.phukrit.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
