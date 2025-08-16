81bac0268fc48463067f3fd518c1331b6bd80319
Mr.Phukrit Kittinontana <phukrit7171@gmail.com>
2025-08-16 23:07:43 +0700

    clearly understand about dto but still not 100%

--- src/main/java/edu/phukrit/learning/controllers/UserController.java
+++ src/main/java/edu/phukrit/learning/controllers/UserController.java
@@ -1,10 +1,15 @@
 package edu.phukrit.learning.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.phukrit.learning.dtos.UserDto;
 import edu.phukrit.learning.services.UserService;
+
 
 @RestController
 @RequestMapping("/api/users")
@@ -12,6 +17,16 @@ public class UserController {
     
     @Autowired
     private UserService userService;
+
+    public UserController() {
+        // Default constructor
+    }
+
+    @GetMapping
+    public List<UserDto> getAllUsers(){
+        return userService.getAllUsersAsDto();
+    }
+    
 
 
 }
--- src/main/java/edu/phukrit/learning/services/UserService.java
+++ src/main/java/edu/phukrit/learning/services/UserService.java
@@ -1,11 +1,14 @@
 package edu.phukrit.learning.services;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import edu.phukrit.learning.dtos.UserDto;
 import edu.phukrit.learning.models.UserModel;
-import edu.phukrit.learning.repositories.UserRepository;  
+import edu.phukrit.learning.repositories.UserRepository;
 
 @Service
 public class UserService {
@@ -15,9 +18,19 @@ public class UserService {
     public UserService() {
         // Default constructor
     }
-    public List<UserModel> getAllUsers() {
-        return userRepository.findAll();
+
+    public List<UserDto> getAllUsersAsDto() {
+        // 1. ไปเอาวัตถุดิบดิบ (UserModel) มาจากคลังเหมือนเดิม
+        List<UserModel> userModels = userRepository.findAll();
+
+        // 2. แปลงร่าง! (หัวใจสำคัญอยู่ตรงนี้)
+        // เราจะวนลูป userModels ทีละตัว แล้วสร้าง UserDto ใหม่จากข้อมูลของมัน
+        List<UserDto> userDtos = userModels.stream()
+                .map(user -> new UserDto(user.getId(), user.getFname(), user.getLname()))
+                .collect(Collectors.toList());
+
+        // 3. คืนค่าเป็นอาหารที่จัดจานสวยงามแล้ว (UserDto)
+        return userDtos;
     }
 
 }
- 
